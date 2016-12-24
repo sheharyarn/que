7e81f0d3d4a613ab26c719ae1adf3a9290548b4d
Sheharyar Naseer <hello@sheharyar.me>
2016-12-24 16:34:01 +0500

    Updated Mixfile with package configurations

--- mix.exs
+++ mix.exs
@@ -1,32 +1,53 @@
 defmodule Que.Mixfile do
   use Mix.Project
 
+  @app     :que
+  @name    "Que"
+  @version "0.1.0-alpha.0"
+  @github  "https://github.com/sheharyarn/#{@app}"
+
   def project do
-    [app: :que,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      # Project
+      app:          @app,
+      version:      @version,
+      elixir:       "~> 1.3",
+      description:  description(),
+      package:      package(),
+      deps:         deps(),
+
+      # ExDoc
+      name:         @name,
+      source_url:   @github,
+      homepage_url: @github,
+      docs: [
+        main:       @name,
+        canonical:  "https://hexdocs.com/#{@app}",
+        extras:     ["README.md"]
+      ]
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:ex_doc, ">= 0.0.0", only: :dev}]
+  end
+
+  defp description do
+    "Background Job Processor for Elixir"
+  end
+
+  defp package do
+    [
+      name: @app,
+      maintainers: ["Sheharyar Naseer"],
+      licenses: ["MIT"],
+      files: ~w(mix.exs lib README.md),
+      links: %{"Github" => @github}
+    ]
   end
 end
+
