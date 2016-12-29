82b0664e1623c516d8bef54d544412e46b3a677c
Sheharyar Naseer <hello@sheharyar.me>
2016-12-29 15:42:04 +0500

    Custom Logger configuration in base Que application

--- lib/que.ex
+++ lib/que.ex
@@ -1,15 +1,27 @@
 defmodule Que do
-  use Application
+  use     Application
+  require Logger
+
+  @prefix "[Que]"
 
   @moduledoc """
   TODO: Add detailed usage docs about the Que package
   """
+
 
   @doc """
   Starts the Que Application (and it's Supervision Tree)
   """
   def start(_type, _args) do
     Que.Supervisor.start_link
+  end
+
+
+  # Logger wrapper for internal Que use. Not meant to be
+  # used as part of the Public API
+  @doc false
+  def __log(msg) do
+    Logger.debug("#{@prefix} #{msg}")
   end
 end
 
