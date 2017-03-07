6793e29a8845cc4cd2858116334edf52d7b1aae5
Sheharyar Naseer <hello@sheharyar.me>
2017-03-06 19:37:57 -0500

    Mnesia methods for creating and finding jobs

--- lib/que/persistence/mnesia.ex
+++ lib/que/persistence/mnesia.ex
@@ -1,26 +1,47 @@
 defmodule Que.Persistence.Mnesia do
   use Amnesia
 
-  @db      DB
-  @table   Jobs
-  @store   Module.concat([__MODULE__, @db, @table])
+  @config [db: DB, table: Jobs]
+
+  @db     Module.concat(__MODULE__, @config[:db])
+  @store  Module.concat(@db, @config[:table])
 
 
   defdatabase DB do
-    deftable Jobs, [:id, :uuid, :arguments, :worker, :status, :ref, :pid], type: :ordered_set do
+    deftable Jobs, [{:id, autoincrement}, :uuid, :arguments, :worker, :status, :ref, :pid],
+      type:  :ordered_set,
+      index: [:uuid, :worker, :ref] do
+
       @store __MODULE__
 
+
       def create_job(job) do
-        Amnesia.transaction(do: @store.write(job))
+        Amnesia.transaction do
+          job
+          |> to_db_job
+          |> @store.write
+          |> to_que_job
+        end
       end
 
+      def find_job(job) do
+        Amnesia.transaction do
+          job.id
+          |> @store.read()
+          |> to_que_job
+        end
+      end
+
+
+      # Convert Que.Job to Mnesia Job
+      defp to_db_job(%Que.Job{} = job) do
+        struct(@store, Map.from_struct(job))
+      end
+
+      # Convert Mnesia Job to Que.Job
+      defp to_que_job(%@store{} = job) do
+        struct(Que.Job, Map.from_struct(job))
+      end
     end
-  end
-
-
-  def insert(job) do
-    @store
-    |> struct(Map.from_struct(job))
-    |> @store.create_job
   end
 end
