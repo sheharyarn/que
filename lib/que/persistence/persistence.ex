e651706d5b249e2d6718026ad7e699d6db9ea3d2
Sheharyar Naseer <hello@sheharyar.me>
2017-03-07 20:38:56 -0500

    Also using defdelegate in Persistence

--- lib/que/persistence/persistence.ex
+++ lib/que/persistence/persistence.ex
@@ -3,34 +3,19 @@ defmodule Que.Persistence do
 
   @adapter Que.Persistence.Mnesia
 
-  @methods %{
-    main:   [:all, :completed, :incomplete],
-    job:    [:find, :insert, :update, :destroy],
-    worker: [:for_worker]
-  }
 
+  # Delegating all methods to the specified adapter
 
-  # Main Methods (No Args)
-  for method <- @methods.main do
-    def unquote(method)() do
-      @adapter.unquote(method)()
-    end
-  end
+  defdelegate find(id),               to: @adapter
+  defdelegate destroy(id),            to: @adapter
 
+  defdelegate insert(job),            to: @adapter
+  defdelegate update(job),            to: @adapter
 
-  # Methods for Jobs
-  for method <- @methods.job do
-    def unquote(method)(%Job{} = job) do
-      @adapter.unquote(method)(job)
-    end
-  end
+  defdelegate all,                    to: @adapter
+  defdelegate completed,              to: @adapter
+  defdelegate incomplete,             to: @adapter
 
-
-  # Methods for Workers
-  for method <- @methods.worker do
-    def unquote(method)(worker) do
-      @adapter.unquote(method)(worker)
-    end
-  end
+  defdelegate for_worker(worker),     to: @adapter
 
 end
