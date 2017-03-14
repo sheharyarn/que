4053891afcc4c468c80ed905c9ae24dda5b75acc
Sheharyar Naseer <hello@sheharyar.me>
2017-03-13 20:38:08 -0400

    Custom Mnesia path with mix env in config

--- config/config.exs
+++ config/config.exs
@@ -21,6 +21,11 @@ use Mix.Config
 #     config :logger, level: :info
 #
 
+
+config :mnesia,
+  dir: 'mnesia/#{Mix.env}/#{node()}'
+
+
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
 # by uncommenting the line below and defining dev.exs, test.exs and such.
