33f0065fa3df149eb3a6d00682d613cfc7b86bad
Sheharyar Naseer <hello@sheharyar.me>
2017-03-16 19:53:17 -0400

    Using Elixir's capture_log to test logged values

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,21 +1,45 @@
 
-# Namespace all test related modules under Que.Test
+## Namespace all test related modules under Que.Test
+
 defmodule Que.Test do
 
-  # A TestWorker for handling Test Jobs
+  ## A TestWorker for handling Test Jobs
+
   defmodule TestWorker do
+    require Logger
     use Que.Worker
 
     def perform(args) do
-      IO.puts("perform: #{inspect(args)}")
+      Logger.debug("perform: #{inspect(args)}")
     end
 
     def on_success(args) do
-      IO.puts("success: #{inspect(args)}")
+      Logger.debug("success: #{inspect(args)}")
     end
 
     def on_failure(args, _err) do
-      IO.puts("failure: #{inspect(args)}")
+      Logger.debug("failure: #{inspect(args)}")
+    end
+  end
+
+
+  ## Helper Module for Tests
+
+  defmodule Helpers do
+    def capture_io(fun) do
+      ExUnit.CaptureIO.capture_io(fn ->
+        fun.()
+      end)
+    end
+
+    def capture_log(level \\ :debug, fun) do
+      Logger.configure(level: level)
+      ExUnit.CaptureIO.capture_io(:user, fn ->
+        fun.()
+        Logger.flush()
+      end)
+    after
+      Logger.configure(level: :debug)
     end
   end
 
