c3785ae79934ac95909409b23cb7267a7b437652
Sheharyar Naseer <hello@sheharyar.me>
2017-03-17 20:25:19 -0400

    Created Success and Failure workers with appropriate callbacks

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,25 +1,51 @@
 
-## Namespace all test related modules under Que.Test
+## Namespace all test related modules under Que.Test.Meta
+## ======================================================
+
 
 defmodule Que.Test.Meta do
+  require Logger
 
-  ## A TestWorker for handling Test Jobs
+
+  # Test workers for handling Jobs
+  # ==============================
 
   defmodule TestWorker do
-    require Logger
     use Que.Worker
 
     def perform(args) do
-      Logger.debug("perform: #{inspect(args)}")
-    end
-
-    def on_success(args) do
-      Logger.debug("success: #{inspect(args)}")
+      Logger.debug("#{__MODULE__} - perform: #{inspect(args)}")
     end
   end
 
 
-  ## Helper Module for Tests
+  defmodule SuccessWorker do
+    use Que.Worker
+    import TestWorker
+
+    def on_success(args) do
+      Logger.debug("#{__MODULE__} - success: #{inspect(args)}")
+    end
+  end
+
+
+  defmodule FailureWorker do
+    use Que.Worker
+
+    def perform do
+      Logger.debug("#{__MODULE__} - perform: #{inspect(args)}")
+      raise "some error"
+    end
+
+    def on_failure(args, _err) do
+      Logger.debug("#{__MODULE__} - failure: #{inspect(args)}")
+    end
+  end
+
+
+
+  # Helper Module for Tests
+  # =======================
 
   defmodule Helpers do
     def capture_io(fun) do
