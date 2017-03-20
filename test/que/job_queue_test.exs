5497ba46735fcfacb487f07330132572b96780bf
Sheharyar Naseer <hello@sheharyar.me>
2017-03-19 21:29:06 -0400

    Tests for JobQueue#process

--- test/que/job_queue_test.exs
+++ test/que/job_queue_test.exs
@@ -1,7 +1,9 @@
 defmodule Que.Test.JobQueue do
   use ExUnit.Case
 
+  alias Que.Job
   alias Que.JobQueue
+  alias Que.Test.Meta.Helpers
   alias Que.Test.Meta.TestWorker
 
 
@@ -62,6 +64,31 @@ defmodule Que.Test.JobQueue do
 
     assert job      == nil
     assert q.queued == []
+  end
+
+
+  test "#process starts the next job in queue and appends it to running" do
+    capture = Helpers.capture_log(fn ->
+      q =
+        TestWorker
+        |> JobQueue.new([Job.new(TestWorker)])
+        |> JobQueue.process
+
+      assert [%Job{status: :started}] = q.running
+      assert [] == q.queued
+    end)
+
+    assert capture =~ ~r/Starting/
+  end
+
+
+  test "#process does nothing when there is nothing in queue" do
+    q_before = JobQueue.new(TestWorker)
+    q_after  = JobQueue.process(q_before)
+
+    assert q_after         == q_before
+    assert q_after.queued  == []
+    assert q_after.running == []
   end
 
 end
