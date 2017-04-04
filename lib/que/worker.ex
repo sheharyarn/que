21be93717482b7d1d497df4d320fc19320e900b2
Sheharyar Naseer <hello@sheharyar.me>
2017-04-04 19:09:32 -0400

    Raising error on Worker compilation if it doesn't export perform method

--- lib/que/worker.ex
+++ lib/que/worker.ex
@@ -1,9 +1,16 @@
 defmodule Que.Worker do
+  @doc false
   defmacro __using__(_opts) do
     quote do
 
-      def perform(_arg) do
+      @after_compile __MODULE__
+
+      def __after_compile__(_env, _bytecode) do
+        unless Module.defines?(__MODULE__, {:perform, 1}) do
+          raise Que.Error.InvalidWorker, "#{ExUtils.Module.name(__MODULE__)} must export a perform/1 method"
+        end
       end
+
 
       def on_success(_arg) do
       end
@@ -11,7 +18,13 @@ defmodule Que.Worker do
       def on_failure(_arg, _err) do
       end
 
-      defoverridable [perform: 1, on_success: 1, on_failure: 2]
+      def __que_worker__ do
+        true
+      end
+
+
+      defoverridable [on_success: 1, on_failure: 2]
     end
   end
+
 end
