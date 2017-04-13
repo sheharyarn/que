17793bfd3588778a2dd13c108e083f8b22280068
Sheharyar Naseer <hello@sheharyar.me>
2017-04-13 18:57:45 -0400

    Creating DB directory on disk before creating DB

--- lib/mix/que.init.ex
+++ lib/mix/que.init.ex
@@ -1,12 +1,24 @@
 defmodule Mix.Tasks.Que.Init do
   use Mix.Task
 
+  @shortdoc "Creates an Mnesia DB on disk for Que"
+
+
   def run(_) do
+    # Get Database Name
     database =
       Que.Persistence.Mnesia.__config__
       |> Keyword.get(:database)
       |> ExUtils.Module.name
 
+
+    # Create the DB directory (if custom path given)
+    if path = Application.get_env(:mnesia, :dir) do
+      :ok = File.mkdir_p!(path)
+    end
+
+
+    # Finally create the Mnesia DB on Disk
     Mix.Tasks.Amnesia.Create.run(~w|-d #{database} --disk|)
   end
 
