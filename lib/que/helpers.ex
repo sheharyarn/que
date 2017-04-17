c1f36c8af5fdb3d155588acbb0b4fbd17af5b83c
Sheharyar Naseer <hello@sheharyar.me>
2017-04-16 21:18:43 -0400

    Docs for Que.Helpers

--- lib/que/helpers.ex
+++ lib/que/helpers.ex
@@ -1,28 +1,30 @@
 defmodule Que.Helpers do
   require Logger
 
+  @moduledoc false
   @prefix "[Que]"
 
 
-  @moduledoc """
-  Helper Module for Que. Exports methods that are used in the
-  project internally. Not meant for used as part of the Public
-  API.
-  """
+  ## Helper Module for `Que`. Exports methods that are used in the
+  ## project internally. Not meant for used as part of the Public
+  ## API.
+
 
 
   @doc """
   Logger wrapper for internal Que use.
   """
-  def log(msg) do
-    Logger.info("#{@prefix} #{msg}")
+  @spec log(message :: String.t) :: :ok
+  def log(message) do
+    Logger.info("#{@prefix} #{message}")
   end
 
 
 
   @doc """
-  Off-loads tasks to custom Que.TaskSupervisor
+  Off-loads tasks to custom `Que.TaskSupervisor`
   """
+  @spec do_task((() -> any)) :: {:ok, pid}
   def do_task(fun) do
     Task.Supervisor.start_child(Que.TaskSupervisor, fun)
   end
