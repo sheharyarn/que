862d1368bf7815d64a32bc4757e79f776a39e12f
Sheharyar Naseer <hello@sheharyar.me>
2017-04-17 22:19:38 -0400

    Documentation for Que.QueueSet

--- lib/que/queue_set.ex
+++ lib/que/queue_set.ex
@@ -2,6 +2,21 @@ defmodule Que.QueueSet do
   defstruct queues: %{}
 
 
+  @moduledoc """
+  Module to manage a QueueSet comprising of multiple `Que.Queue`s,
+  each for their respective `Que.Worker.t`.
+
+  Maintains a collection for Queues, one per `Que.Worker`, and
+  provides wrappers around it for managing Jobs. Checks a given
+  `Que.Job`'s type and off-loads the operation to the proper Queue
+  and Worker pair.
+
+  There should be only one `QueueSet` per Que application.
+  Meant for internal usage, so you shouldn't use this unless you
+  absolutely know what you're doing.
+  """
+
+
   @typedoc  "A `Que.QueueSet` struct"
   @type     t :: %Que.QueueSet{}
 
@@ -10,6 +25,7 @@ defmodule Que.QueueSet do
   @doc """
   Returns a new QueueSet with defaults
   """
+  @spec new :: Que.QueueSet.t
   def new do
     %Que.QueueSet{}
   end
@@ -20,6 +36,7 @@ defmodule Que.QueueSet do
   Finds the Queue for a specified worker. If the queue does not
   exist, returns a new Queue for that worker.
   """
+  @spec get(queue_set :: Que.QueueSet.t, worker :: Que.Worker.t) :: Que.Queue.t
   def get(%Que.QueueSet{} = qset, worker) do
     qset.queues[worker] || Que.Queue.new(worker)
   end
@@ -29,6 +46,7 @@ defmodule Que.QueueSet do
   @doc """
   Adds a Job to the appropriate Queue in a QueueSet
   """
+  @spec add(queue_set :: Que.QueueSet.t, job :: Que.Job.t) :: Que.QueueSet.t
   def add(%Que.QueueSet{} = qset, %Que.Job{} = job) do
     q =
       qset
@@ -44,6 +62,7 @@ defmodule Que.QueueSet do
   Finds a Job in the QueueSet by the given Job's id and updates
   (replaces) it with the specified Job
   """
+  @spec update(queue_set :: Que.QueueSet.t, job :: Que.Job.t) :: Que.QueueSet.t
   def update(%Que.QueueSet{} = qset, %Que.Job{} = job) do
     q =
       qset
@@ -58,6 +77,7 @@ defmodule Que.QueueSet do
   @doc """
   Removes the specified job from the appropriate Queue's running list
   """
+  @spec remove(queue_set :: Que.QueueSet.t, job :: Que.Job.t) :: Que.QueueSet.t
   def remove(%Que.QueueSet{} = qset, %Que.Job{} = job) do
     q =
       qset
@@ -72,6 +92,7 @@ defmodule Que.QueueSet do
   @doc """
   Calles :process on all Queues in QueueSet
   """
+  @spec process(queue_set :: Que.QueueSet.t) :: Que.QueueSet.t
   def process(%Que.QueueSet{ queues: queues } = qset) do
     queues =
       queues
@@ -87,6 +108,7 @@ defmodule Que.QueueSet do
   Groups a list of Jobs into a proper QueueSet. All Jobs are loaded
   only in the :queued list
   """
+  @spec collect(jobs :: list(Que.Job.t)) :: Que.QueueSet.t
   def collect(jobs) when is_list(jobs) do
     queues =
       jobs
@@ -104,6 +126,7 @@ defmodule Que.QueueSet do
 
   If no key is specified, it's assumed to be `:id`.
   """
+  @spec find(queue_set :: Que.QueueSet.t, key :: atom, value :: term) :: Que.Job.t | nil
   def find(%Que.QueueSet{ queues: queues }, key \\ :id, value) do
     Enum.find_value(queues, fn {_, q} ->
       Que.Queue.find(q, key, value)
