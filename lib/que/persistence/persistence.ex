3036ba6f4003f59034c3419a338be0f5485716bd
Sheharyar Naseer <hello@sheharyar.me>
2017-04-20 21:42:12 -0400

    Typespecs and moduledoc for Persistence

--- lib/que/persistence/persistence.ex
+++ lib/que/persistence/persistence.ex
@@ -1,22 +1,80 @@
 defmodule Que.Persistence do
+  @moduledoc """
+  Provides a high-level API to interact with Jobs in Database
+
+  This module is a behaviour that delegates calls to the specified
+  adapter. It has been designed in a way that it's easy to write
+  custom adapters for other Databases or Stores like Redis, even
+  though there are no current plans on supporting any thing other
+  than `Mnesia`.
+  """
+
+
+  ## Adapter to delegate all methods to
   @adapter Que.Persistence.Mnesia
 
 
-  # Delegating all methods to the specified adapter
 
-  defdelegate find(id),               to: @adapter
-  defdelegate destroy(id),            to: @adapter
 
-  defdelegate insert(job),            to: @adapter
-  defdelegate update(job),            to: @adapter
+  @doc """
+  """
+  @spec find(id :: integer) :: Que.Job.t
+  defdelegate find(id), to: @adapter
 
-  defdelegate all,                    to: @adapter
-  defdelegate completed,              to: @adapter
-  defdelegate incomplete,             to: @adapter
-  defdelegate failed,                 to: @adapter
 
-  defdelegate for_worker(worker),     to: @adapter
-  defdelegate initialize,             to: @adapter
+
+
+  @spec destroy(id :: integer) :: :ok | no_return
+  defdelegate destroy(id), to: @adapter
+
+
+
+
+  @spec insert(job :: Que.Job.t) :: Que.Job.t
+  defdelegate insert(job), to: @adapter
+
+
+
+
+  @spec update(job :: Que.Job.t) :: Que.Job.t
+  defdelegate update(job), to: @adapter
+
+
+
+
+  @spec all :: list(Que.Job.t)
+  defdelegate all, to: @adapter
+
+
+
+
+  @spec completed :: list(Que.Job.t)
+  defdelegate completed, to: @adapter
+
+
+
+
+  @spec incomplete :: list(Que.Job.t)
+  defdelegate incomplete, to: @adapter
+
+
+
+
+  @spec failed :: list(Que.Job.t)
+  defdelegate failed, to: @adapter
+
+
+
+
+  @spec for_worker(worker :: Que.Worker.t) :: list(Que.Job.t)
+  defdelegate for_worker(worker), to: @adapter
+
+
+
+
+  @spec initialize :: :ok | :error
+  defdelegate initialize, to: @adapter
+
 
 
 
